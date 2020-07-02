6533d6fc7c2b559619b191b4b947617a6069253d
Justin Caudill <jcaudill_2003@yahoo.com>
2020-07-02 13:08:52 -0400

    Next commit

---
+++ scripts/events.js
@@ -0,0 +1,19 @@
+addButton.addEventListener('click', function () {
+    const x = Number (xInput.value)
+    const y = Number (yInput.value)
+
+    outputElement.innerHTML = calculator(x, y, add)
+})
+
+multiplyButton.addEventListener('click', function () {
+    const x = Number (xInput.value)
+    const y = Number (yInput.value)
+
+    outputElement.innerHTML = calculator(x, y, multiply)
+})
+divideButton.addEventListener('click', function () {
+    const x = Number (xInput.value)
+    const y = Number (yInput.value)
+
+    outputElement.innerHTML = calculator(x, y, divide)
+})
